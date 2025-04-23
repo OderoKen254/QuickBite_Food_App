2d22dfe34a0b6ecd69f3a0ada136a5a8f66a5478
Priscillah Njai <priscillahnjaii@gmail.com>
2025-04-24 00:44:11 +0300

    Adds loader

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,5 @@
 import React, { useState } from 'react'
-import Navbar from 'copmponents/layout/Navbar'
-import Footer from 'components/layout/Footer'
-import Loader from 'components/layout/Loader'
+
 
 function App() {
   return (
---
+++ src/components/core-components/Footer.jsx
@@ -0,0 +1,5 @@
+import React from 'react';
+
+export default function Footer() {
+    <div>Footer</div>
+}
