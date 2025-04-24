92605f5f202e3e4885b22e3370a405cba2ba4acf
Aquila Jedidiah <jedaqsaul@gmail.com>
2025-04-24 09:44:52 +0300

    sets cart Items and each item quantity as 1

--- src/components/cart-checkout flow/Cart.jsx
+++ src/components/cart-checkout flow/Cart.jsx
@@ -1,12 +1,18 @@
-import React, { useEffect } from "react";
+import React, { useEffect, useState } from "react";
 
 export default function Cart() {
+  const [cartItems, setCartItems] = useState([]);
   useEffect(() => {
     fetch("http://localhost:3000/cart")
       .then((res) => res.json())
       .then((data) => {
-        console.log(data);
-      });
+        const updatedData = data.map((item) => ({
+          ...item,
+          quantity: 1,
+        }));
+        setCartItems(updatedData);
+      })
+      .catch((error) => console.log(error));
   }, []);
 
   return <div className="cart-container"></div>;
