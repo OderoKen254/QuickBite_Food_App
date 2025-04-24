9862179af497ad50c09430ea707c1b50aade9b57
Aquila Jedidiah <jedaqsaul@gmail.com>
2025-04-24 12:57:35 +0300

    Adds styles to price-item

--- src/components/cart-checkout flow/CartItem.jsx
+++ src/components/cart-checkout flow/CartItem.jsx
@@ -8,7 +8,7 @@ export default function CartItem({ item, onIncrease, onDecrease, onDelete }) {
       <div className="cart-item-details">
         <h3 className="cart-item-name">{item.name}</h3>
         <p className="cart-item-description">{item.description}</p>
-        <h4 className="item-item-price">Ksh{item.price}</h4>
+        <h4 className="item-price">Ksh{item.price}</h4>
       </div>
       <div className="quantity-controls">
         <button className="quantity-button" onClick={onDecrease}>
