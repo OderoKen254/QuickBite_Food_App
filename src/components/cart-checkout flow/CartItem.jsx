edce2cdb5e527f08d2776fd205f84cff5464b8fc
Aquila Jedidiah <jedaqsaul@gmail.com>
2025-04-24 10:15:12 +0300

    create cartItem component skeleton

--- src/components/cart-checkout flow/CartItem.jsx
+++ src/components/cart-checkout flow/CartItem.jsx
@@ -1,5 +1,5 @@
 import React, { useState } from "react";
 
-export default function CartItem() {
-  return <div></div>;
+export default function CartItem({ item, onIncrease, onDecrease, onDelete }) {
+  return <div className="cart-item">{/* enter cart item details here */}</div>;
 }
