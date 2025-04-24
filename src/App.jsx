42e6767bc9bee3032fe48841fd0a7f2da4fb6e2e
Priscillah Njai <priscillahnjaii@gmail.com>
2025-04-24 04:02:17 +0300

    Adds media scren specs

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,13 @@
 import React, { useState } from 'react'
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+import NavBar from './components/core-components/NavBar';
+import Footer from './components/core-components/Footer';
+import Loader from './components/core-components/Loader';
 
 
 function App() {
+
+  // const [loading, setLoading] = useState(true);
   return (
     <div>
       <h1>Welcome to QuickBite!</h1>
