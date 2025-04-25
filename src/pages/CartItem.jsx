9e597821beee3027ac7cded4026733c13ce946d7
Aquila Jedidiah <jedaqsaul@gmail.com>
2025-04-26 01:04:39 +0300

    render item name, description and price

--- src/pages/CartItem.jsx
+++ src/pages/CartItem.jsx
@@ -1,5 +1,13 @@
 import React from "react";
 
 export default function CartItem({ item, onIncrease, onDecrease, onDelete }) {
-  return <div></div>;
+  return (
+    <div>
+      <div>
+        <h3>{item.name}</h3>
+        <p>{item.description}</p>
+        <h4>Ksh {item.price}</h4>
+      </div>
+    </div>
+  );
 }
